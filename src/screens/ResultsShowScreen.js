506261056522167488b123374695ac474b1fe6e9
fahmichibani <fahmichibani@gmail.com>
2020-04-02 05:29:00 +0100

    mise a jour sdk

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -5,7 +5,7 @@ import yelp from '../api/yelp';
 const ResultsShowScreen = ({ navigation }) => {
   const [result, setResult] = useState(null);
   const id = navigation.getParam('id');
-
+console.log(navigation)
   const getResult = async (id) => {
     const response = await yelp.get(`/${id}`);
     setResult(response.data);
